fd27d6b91833daaaafd2d7e88bf4fc94f632f89d
Wahid Ejaz <wahidejaz@Wahids-MacBook-Air.local>
2025-08-31 14:34:29 +0500

    Project initialization

---
+++ src/modules/server.ts
@@ -0,0 +1,9 @@
+import { Server } from "socket.io";
+
+const io = new Server(3000, { /* options */ });
+
+io.on("connection", (socket) => {
+    console.log('new connection')
+});
+
+module.exports={socket: io};
