495f8e882e8cb82b39c785af6c3dd84f2ae38d42
Wahid Ejaz <wahidejaz@Wahids-MacBook-Air.local>
2025-09-01 10:53:11 +0500

    Added redirect handler

--- src/components/homepage_clientside.tsx
+++ src/components/homepage_clientside.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from 'next/link'
 export default function Home(){
 
     let server_handler = (ev: any) => {
@@ -9,7 +10,9 @@ export default function Home(){
     }
     return(
         <div className="flex justify-center items-center w-screen h-screen">
-            <button className="p-3 cursor-pointer" onClick={server_handler}>start server</button>
+            <Link href="/startserver">
+                <button className="p-3 cursor-pointer" onClick={server_handler}>start server</button>
+            </Link>
             <button className="p-3 cursor-pointer" onClick={client_handler}>join server</button>
         </div>
     )
