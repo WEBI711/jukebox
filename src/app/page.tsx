a732e597c7e8d542630404e5f5fabb272048fbf4
Wahid Ejaz <wahidejaz@Wahids-MacBook-Air.local>
2025-09-01 10:52:33 +0500

    Removed testing code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import CHome from '@/components/homepage_clientside';
-import serverlist from '@/modules/serverlist';
 export default function Home(){
-    serverlist.add_server()
-    serverlist.add_server()
-    let list = serverlist.get_list()
     return(
         <div className="flex justify-center items-center w-screen h-screen">
             <CHome/>
