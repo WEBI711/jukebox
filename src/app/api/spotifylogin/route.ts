c9e1ea4b2f1a55909070797e4441ae0d747e1fbd
Wahid Ejaz <wahidejaz@Wahids-MacBook-Air.local>
2025-09-01 10:52:15 +0500

    Added spotifylogin route to handle redirect to spotify authentication endpoint

---
+++ src/app/api/spotifylogin/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+
+export async function Get(request: Request){
+    
+}
