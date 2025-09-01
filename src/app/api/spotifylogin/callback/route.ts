f40c64a0a668026be6d565a055ba5bda9a9e0919
Wahid Ejaz <wahidejaz@Wahids-MacBook-Air.local>
2025-09-01 12:32:57 +0500

    Correctly extracting code and state requesst params from redirect

--- src/app/api/spotifylogin/callback/route.ts
+++ src/app/api/spotifylogin/callback/route.ts
@@ -3,6 +3,8 @@ import { redirect, RedirectType } from 'next/navigation'
 
 export async function GET(req: NextRequest){
     const searchParams = req.nextUrl.searchParams
-    const query = searchParams.get('query')
+    const code = searchParams.get('code')
+    // TODO: Logic to check this against initail state to protect agains xsr attacks
+    const state = searchParams.get('state')
     redirect('/', RedirectType.push)
 }
