e3155e716565e7e8a9668a26d5bd8f10588b87f6
Wahid Ejaz <wahidejaz@Wahids-MacBook-Air.local>
2025-09-03 12:41:41 +0500

    Updated home page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
 import CHome from '@/components/homepage_clientside';
+import {TypewriterEffectSmooth} from '@/components/ui/typewriter-effect'
+import Link from 'next/link'
 export default function Home(){
+    const words = [
+        {
+            text: "Welcome",
+        },
+        {
+            text: "to",
+        },
+        {
+            text: "JukeBox.",
+            className: "text-blue-500 dark:text-blue-500",
+        },
+    ];
     return(
-        <div className="flex justify-center items-center w-screen h-screen">
-            <CHome/>
+        <div className="flex flex-col items-center justify-center h-[40rem]">
+            <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
+                Play your own music
+            </p>
+            <TypewriterEffectSmooth words={words} />
+            <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+                <Link href="/startserver">
+                    <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+                        Create Server
+                    </button>
+                </Link>
+                <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+                    Join Server
+                </button>
+            </div>
         </div>
     )
 }
