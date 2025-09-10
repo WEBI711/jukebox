c01cd207b47430d3756b22bad94581d88ff113af
Wahid Ejaz <wahidejaz@Wahids-MacBook-Air.local>
2025-09-10 23:17:37 +0500

    Added shadcn components

--- src/app/room/page.tsx
+++ src/app/room/page.tsx
@@ -1,8 +1,12 @@
 import room_list from '@/modules/roomList'
 import SpotifyPlayer from '@/components/spotifyplayer'
+import {Button} from '@/components/ui/button';
+import {Slider} from '@/components/ui/slider';
+import { cn } from "@/lib/utils"
 type roomProps = {
     searchParams: {room_id: string,}
 }
+type SliderProps = React.ComponentProps<typeof Slider>
 export default async function Room({searchParams}: roomProps){
     searchParams = await searchParams;
     console.log(room_list.get_list())
@@ -11,8 +15,9 @@ export default async function Room({searchParams}: roomProps){
     if(access_token){
         return(
             <div>
-                <div>Welcome to room</div>
+                <Button>Welcome to room</Button>
                 <SpotifyPlayer access_token={access_token} />
+                <Slider defaultValue={[33]} max={100} step={1} className='w-50'/>
             </div>
         )
     }
