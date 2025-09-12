f605fef068d4edcdc81d8f44fe79cd7e873ff9ef
Wahid Ejaz <wahidejaz@Wahids-MacBook-Air.local>
2025-09-12 16:48:55 +0500

    Updated server side logic for client side room component

--- src/app/room/page.tsx
+++ src/app/room/page.tsx
@@ -1,17 +1,8 @@
 import room_list from '@/modules/roomList'
-import SpotifyPlayer from '@/components/spotifyplayer'
-import { ScrollArea } from "@/components/ui/scroll-area"
+import RoomClient from '@/components/room_client'
 import tracksobject from 'public/mocktracks'
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Separator } from "@/components/ui/separator"
-                                    
 type roomProps = {
     searchParams: {room_id: string,}
-}
-type imageType = {
-    url: string,
-    width: number,
-    height: number
 }
 export default async function Room({searchParams}: roomProps){
     searchParams = await searchParams;
@@ -21,33 +12,7 @@ export default async function Room({searchParams}: roomProps){
     if(access_token){
         return(
             <div className='flex items-center justify-center size-full'>
-                <SpotifyPlayer access_token={access_token} />
-                <ScrollArea className="h-3/4 w-1/2 rounded-md border p-4">
-                    <div className='size-full overflow-hidden'>
-                        {tracksobject.tracks.items.map((item: any) => {
-                            let img = item?.album?.images.reduce((smallest: imageType, curr: imageType) => {
-                                if(curr.width < smallest.width)
-                                    return curr
-                                return smallest
-                            })
-                            return (
-                                <div className='w-full h-auto'>
-                                    <div className='flex justify-start items-center w-full min-h-[100px] my-1 p-3 rounded-2xl'>
-                                        <Avatar>
-                                            <AvatarImage src={img.url}></AvatarImage>
-                                            <AvatarFallback>Song</AvatarFallback>
-                                        </Avatar>
-                                        <div className='p-3 overflow-hidden'>
-                                            <p className='text-xl'>{item.name}</p>
-                                            <p className='text-xs'>{item.album.name}</p>
-                                        </div>
-                                    </div>
-                                    <Separator/>
-                                </div>
-                            )
-                        })}
-                    </div>
-                </ScrollArea>
+                <RoomClient access_token={access_token} tracks={tracksobject.tracks} server_socket_port={searchParams.room_id}/>
             </div>
         )
     }
