2db13b8e840b64de548ae5c34d5d89ff677cd051
Wahid Ejaz <wahidejaz@Wahids-MacBook-Air.local>
2026-01-14 13:01:56 +0500

    simplified auth flow requests

--- src/app/api/spotifylogin/callback/route.ts
+++ src/app/api/spotifylogin/callback/route.ts
@@ -6,35 +6,32 @@ import { redirect, RedirectType } from 'next/navigation'
 import room from '@/modules/room';
 
 export async function GET(req: NextRequest) {
+  let room;
+  try {
+    // TODO: Logic to check this against initail state to protect agains xsr attacks
     const searchParams = req.nextUrl.searchParams
     const code = searchParams.get('code');
-    // TODO: Logic to check this against initail state to protect agains xsr attacks
+    if (!code) throw new Error("Spotify endpoint did not return an authcode")
     const state = searchParams.get('state')
-
-    let room = null;
-    try {
-        if (code) {
-            room = await create_new_room(code);
-        }
-    } catch (err) {
-        console.log(err)
-        return NextResponse.json({ error: err })
-    }
-
-    if (room)
-        return redirect(`/room?room_id=${room.room_id}`, RedirectType.push)
-    else
-        return NextResponse.json({ error: 'unable to create room' })
+    room = await create_new_room(code);
+    if (!room) throw new Error("Room creation unsuccessful")
+  } catch (err) {
+    console.log("Error in callback spotify callback endpoint")
+    console.log(err)
+    return NextResponse.json({ error: 'error in callback' }, { status: 500 })
+  }
+  // Must be outside of try catch block
+  return redirect(`/room?room_id=${room.room_id}`, RedirectType.push)
 }
 
 // --- Helpers ---
 async function create_new_room(spotify_auth_code: string) {
-    const room_id = crypto.randomUUID();
-    const token_info = await spotifyHandler.token_request(spotify_auth_code)
-    if (token_info) {
-        let new_room = new room(room_id, token_info)
-        room_list.add(new_room)
-        return new_room;
-    }
-    return null
+  const room_id = crypto.randomUUID();
+  const token_info = await spotifyHandler.token_request(spotify_auth_code)
+  if (token_info) {
+    let new_room = new room(room_id, token_info)
+    room_list.add(new_room)
+    return new_room;
+  }
+  return null
 }
--- src/app/api/spotifylogin/route.ts
+++ src/app/api/spotifylogin/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import spotifyHandler from '@/modules/spotifyHandler'
 import { error } from "console";
 
-export async function GET(req: Request){
+export async function GET(req: Request) {
     spotifyHandler.auth_code_request()
-    return NextResponse.json({test: 'test'})
+    return NextResponse.json({ test: 'test' })
 }
 
