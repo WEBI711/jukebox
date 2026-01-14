dc269f975561a6c2745f979c20e3701704c0e3dc
Wahid Ejaz <wahidejaz@Wahids-MacBook-Air.local>
2026-01-14 13:02:27 +0500

    redirect to spotify login straight from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 import HomePageButtons from "@/components/homepage-buttons";
 import server from "@/modules/socketio_server";
+import { redirect } from 'next/navigation'
 export default function Home() {
-  if (server.check()) console.log("Server socket is up and running");
-  const words = [
-    { text: "Welcome" },
-    { text: "to" },
-    { text: "JukeBox.", className: "text-blue-500 dark:text-blue-500" },
-  ];
-  return (
-    <div className="flex flex-col items-center justify-center h-[40rem]">
-      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
-        Play your own music
-      </p>
-      <TypewriterEffectSmooth words={words} />
-      <HomePageButtons />
-    </div>
-  );
+  redirect('/api/spotifylogin');
+  //if (server.check()) console.log("Server socket is up and running");
+  //const words = [
+  //  { text: "Welcome" },
+  //  { text: "to" },
+  //  { text: "JukeBox.", className: "text-blue-500 dark:text-blue-500" },
+  //];
+  //return (
+  //  <div>
+  //    Hello world!
+  //  </div>
+  //);
 }
